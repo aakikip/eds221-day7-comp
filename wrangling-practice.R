cbb98c1bc3abcfb5ad6d707bc39eaeccea543bb2
aakikip <poudelaakriti@gmail.com>
2025-08-19 11:46:37 -0700

    wrangling-practice

--- wrangling-practice.R
+++ wrangling-practice.R
@@ -36,6 +36,63 @@ penguins |>
 
 
 
+penguins |>
+  filter(species == "Adelie") |>
+  filter(!is.na(flipper_length_mm),
+         !is.na(sex)) |> #remove rows that are not NA
+  group_by(sex) |>
+  summarise(mean = mean(flipper_length_mm),
+            standard_dev = sd(flipper_length_mm),
+            sample_size = n())
+
+#Wrangling continued - joins of different flavors
+
+#Practice with joins
+
+animals <- data.frame(
+  stringsAsFactors = FALSE,
+          location = c("lagoon", "bluff", "creek", "oaks", "bluff"),
+           species = c("bobcat", "coyote", "fox", "squirrel", "bobcat"),
+          maturity = c("adult", "juvenile", "adult", "juvenile", "adult")
+)
+
+sites <- data.frame(
+  stringsAsFactors = FALSE,
+          location = c("beach", "lagoon", "bluff", "oaks"),
+    full_site_name = c("Goleta Beach","UCSB Lagoon",
+                       "Ellwood Mesa","Fremont Campground"),
+      jurisdiction = c("SB City", "UCSB", "SB City", "USFS")
+)
+
+#Practice with full join
+#keeps all rows and adds all columns
+full_join(animals, sites)
+
+#left join
+left_join(animals, sites)
+
+#right join
+right_join(animals, sites)
+
+#inner join
+inner_join(animals, sites)
+
+#Filtering joins
+semi_join(animals, sites)
+
+animals |>
+  filter(location %in% sites$location)
+
+#anti join
+anti_join(animals, sites)
+
+animals |>
+  filter(!location %in% sites$location)
+
+anti_join(sites, animals)
+
+
+
 
 
 
